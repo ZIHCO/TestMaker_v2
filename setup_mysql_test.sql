a3dc1f875b85ed26268207b711ac59195886c862
ZIHCO <chizobajames21@gmail.com>
2023-11-04 18:13:33 +0100

    done

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,6 +1,6 @@
 -- prepares MySQL server for the project
 
-CREATE USER IF NOT EXISTS 'testmaker_test'@'localhost' IDENTIFIED BY 'Peace(1)wq';
+CREATE USER IF NOT EXISTS 'testmaker_test'@'localhost' IDENTIFIED BY 'testmaker_test_pwd';
 CREATE DATABASE IF NOT EXISTS testmaker_test_db;
 GRANT ALL PRIVILEGES ON testmaker_test_db.* TO 'testmaker_test'@'localhost';
 GRANT SELECT ON performance_schema.* TO 'testmaker_test'@'localhost';
